80a2949169cf9493a01151aba8da59cdd16add53
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-01-30 15:13:43 +0100

    first example

---
+++ src/main/scala/FirstExample.scala
@@ -0,0 +1,35 @@
+
+/**
+ * Simple Domain show-case
+ */
+object FirstExample {
+
+  type User = String
+
+  trait SocialService {
+    def getFollowerStatsByUser(u: User): FollowerStats
+  }
+
+  case class FollowerStats(nrFollowers: Int, nrFollowing: Int)
+
+  case class Cache(
+    statistics: Map[User, FollowerStats],
+    hits: Int,
+    miss: Int) {
+
+    def get(u: User): Option[FollowerStats] = statistics.get(u)
+
+    def update(u: User, fs: FollowerStats): Cache = copy(statistics = statistics + (u -> fs))
+
+  }
+
+}
+
+object App extends App {
+  import FirstExample._
+
+  object FakeSocialService extends SocialService {
+    override def getFollowerStatsByUser(u: User): FollowerStats = FollowerStats(0,0)
+  }
+
+}
