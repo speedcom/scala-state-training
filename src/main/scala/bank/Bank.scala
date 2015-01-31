464b7ded24008291f8420b5cdf32f0b7cbf8c2e5
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-01-31 11:19:51 +0100

    State action with its map functions and example of using

--- src/main/scala/bank/Bank.scala
+++ src/main/scala/bank/Bank.scala
@@ -41,5 +41,36 @@ object Bank_2 {
  * some computation and produces an A and s new S.
  */
 case class State[S,A](run: S => (A,S)) {
-  
+
+  /*
+  map builds a new state action that, once run, has its output
+  run through f, converting it from A to B
+   */
+  def map[B](f: A => B): State[S, B] =
+    State { run andThen { case (a, s) => (f(a), s) }}
+
+
 }
+
+// Composing with pure functions
+object StateMapExample extends App {
+
+  type Account = List[Float]
+  type Tx[A] = State[Account, A]
+
+  val balance: Tx[Float] = State { account =>
+    (account.sum, account)
+  }
+
+  val report: Tx[String] = balance map { sum => "Your balance is " + sum }
+
+}
+
+
+
+
+
+
+
+
+
